b3a3811aa6bd5fe9907e95443360ba01a9ab1a7b
Olabi Elore <olabielore@gmail.com>
2025-11-20 11:40:40 +0200

    my js hm-10 fixed problems

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,54 +1,9 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.createElement('form');
-form.classList.add('form');
-
-const labelDelay = document.createElement('label');
-labelDelay.textContent = 'Delay (ms)';
-
-const inputDelay = document.createElement('input');
+const form = document.querySelector('.form');
+const inputDelay = form.elements.delay;
 inputDelay.classList.add('input-delay');
-inputDelay.type = 'number';
-inputDelay.name = 'delay';
-inputDelay.required = true;
-
-const fieldset = document.createElement('fieldset');
-
-const legend = document.createElement('legend');
-legend.textContent = 'State';
-
-const labelFulfilled = document.createElement('label');
-labelFulfilled.textContent = 'Fulfilled';
-
-const inputFulfilled = document.createElement('input');
-inputFulfilled.type = 'radio';
-inputFulfilled.name = 'state';
-inputFulfilled.value = 'fulfilled';
-inputFulfilled.required = true;
-
-const labelRejected = document.createElement('label');
-labelRejected.textContent = 'Rejected';
-
-const inputRejected = document.createElement('input');
-inputRejected.type = 'radio';
-inputRejected.name = 'state';
-inputRejected.value = 'rejected';
-inputRejected.required = true;
-
-const button = document.createElement('button');
-button.type = 'submit';
-button.textContent = 'Create notification';
-
-labelDelay.append(inputDelay);
-labelFulfilled.append(inputFulfilled);
-labelRejected.append(inputRejected);
-fieldset.append(legend, labelFulfilled, labelRejected);
-form.append(labelDelay, fieldset, button);
-
-document.body.append(form);
-
-console.log(form);
 
 const onSubmit = event => {
   event.preventDefault();
